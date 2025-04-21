e314819d509fd0b2a7865b7a55bb505843d2cff1
Maria Eduarda Bergamin Jorge <mariabergamin18@gmail.com>
2025-04-21 14:26:13 -0300

    commit game over screen

--- Documents/github/elite-penman-godotgame/Main.gd
+++ Documents/github/elite-penman-godotgame/Main.gd
@@ -5,16 +5,20 @@ var Enemy = preload("res://Enemy.tscn")
 onready var enemy_container = $EnemyContainer
 onready var spawn_container = $SpawnContainer
 onready var spawn_timer = $SpawnTimer
+onready var difficulty_timer = $DifficultyTimer
+
+onready var difficulty_value = $CanvasLayer/VBoxContainer/BottomRow/HBoxContainer/DifficultyValue
+onready var killed_value = $CanvasLayer/VBoxContainer/TopRow2/TopRow/EnemiesKilledValue
+onready var game_over_screen = $CanvasLayer/GameOVerScreen
 
 var active_enemy = null
 var current_letter_index : int = -1
 
 var difficulty: int = 0
+var enemies_killed: int = 0
 
 func _ready() -> void:
-	randomize()
-	spawn_timer.start()
-	spawn_enemy()
+	start_game()
 
 func find_new_active_enemy(typed_character: String):
 	for enemy in enemy_container.get_children():
@@ -47,6 +51,8 @@ func _unhandled_input(event: InputEvent) -> void:
 					current_letter_index = -1
 					active_enemy.queue_free()
 					active_enemy = null
+					enemies_killed += 1
+					killed_value.text = str(enemies_killed) 
 			else:
 				print("incorrectly typed %s instead of %s" % [key_typed, next_character])
 
@@ -58,8 +64,8 @@ func spawn_enemy():
 	var enemy_instance = Enemy.instance()
 	var spawns = spawn_container.get_children()
 	var index = randi() % spawns.size()
-	enemy_container.add_child(enemy_instance)
 	enemy_instance.global_position = spawns[index].global_position
+	enemy_container.add_child(enemy_instance)
 	enemy_instance.set_difficulty(difficulty)
 
 func _on_DifficultyTimer_timeout():
@@ -68,5 +74,36 @@ func _on_DifficultyTimer_timeout():
 	print("Difficulty increased to %d " % difficulty)
 	var new_wait_time = spawn_timer.wait_time -0.2
 	spawn_timer.wait_time = clamp(new_wait_time, 1, spawn_timer.wait_time)
+	difficulty_value.text = str(difficulty)
 
 
+
+
+func _on_LoseArea_body_entered(body):
+	game_over()
+	
+
+
+func game_over():
+	game_over_screen.show()
+	spawn_timer.stop()
+	difficulty_timer.stop()
+	active_enemy = null
+	current_letter_index = -1
+	for enemy in enemy_container.get_children():
+		enemy.queue_free()
+
+
+func start_game():
+	game_over_screen.hide()
+	difficulty = 0
+	enemies_killed = 0
+	randomize()
+	spawn_timer.start()
+	difficulty_timer.start()
+	spawn_enemy()
+
+
+
+func _on_RestartButton_pressed():
+	start_game()
